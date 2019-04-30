a06d424fcf3fc4c6738300d8feeffcbff118b13c
Agustin Caputo <agustin.caputo@gmail.com>
2019-04-30 20:22:50 -0300

    Update Bomba.java

--- src/bomberman/Bomba.java
+++ src/bomberman/Bomba.java
@@ -5,10 +5,14 @@ public class Bomba extends Entidad{
 	private int rango;
 	private int tiempoDeEjecucion;
 	
-	
 	public Bomba(int x, int y) {
 		super(x,y);
 		this.rango=1;
 		this.tiempoDeEjecucion=3;		
+	}
+	
+	
+	public void explotarBomba() {
+		
 	}
 }
